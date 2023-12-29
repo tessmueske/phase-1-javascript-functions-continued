f5424af1c7b4d039cbd0d6d46c60eb2d651d4e80
Tess <tess.mueske@gmail.com>
2023-12-29 11:31:40 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+const mondayWork = function (responsibility = "go to the office") {
+    return (`This Monday, I will ${responsibility}.`)
+}
+
+function wrapAdjective(parameter = "a dedicated programmer") {
+    return function(flair = "*") {
+      return "You are " + parameter + flair + parameter + "!";
+    };
+  }
